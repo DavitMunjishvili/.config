0ab9e75011fcb10e50555dd08a1325d950d50b9f
Davit Munjishvili <drummerthree@gmail.com>
2025-04-09 23:10:40 +0400

    fix prompt selection and added prev cmd duration

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-fish_config prompt choose arrow
-
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
