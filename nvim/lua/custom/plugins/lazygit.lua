3778296afbf03b7d1f4ce57770ac65288467e87f
Davit Munjishvili <drummerthree@gmail.com>
2025-04-09 23:11:08 +0400

    reduce lazygit window blend to 0

--- nvim/lua/custom/plugins/lazygit.lua
+++ nvim/lua/custom/plugins/lazygit.lua
@@ -15,7 +15,7 @@ return {
   },
   config = function()
     require('telescope').load_extension 'lazygit'
-    vim.g.lazygit_floating_window_winblend = 20 -- transparency of floating window
+    vim.g.lazygit_floating_window_winblend = 0 -- transparency of floating window
     vim.g.lazygit_floating_window_scaling_factor = 1 -- scaling factor for floating window
   end,
   keys = {
