a387e96e25a70261abb1724f04592f1737c25748
Data Munjishvili <drummerthree@gmail.com>
2025-04-22 15:58:32 +0400

    remove comments

--- nvim/lua/custom/plugins/lazygit.lua
+++ nvim/lua/custom/plugins/lazygit.lua
@@ -8,15 +8,14 @@ return {
     'LazyGitFilter',
     'LazyGitFilterCurrentFile',
   },
-  -- optional for floating window border decoration
   dependencies = {
     'nvim-telescope/telescope.nvim',
     'nvim-lua/plenary.nvim',
   },
   config = function()
     require('telescope').load_extension 'lazygit'
-    vim.g.lazygit_floating_window_winblend = 0 -- transparency of floating window
-    vim.g.lazygit_floating_window_scaling_factor = 1 -- scaling factor for floating window
+    vim.g.lazygit_floating_window_winblend = 0
+    vim.g.lazygit_floating_window_scaling_factor = 1
   end,
   keys = {
     { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
