2527ac4fc163abdeb0070df378672372adb3d3b8
Data Munjishvili <drummerthree@gmail.com>
2025-05-07 15:01:53 +0400

    move `bun` to separate file

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,3 @@ set -U EDITOR nvim
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
