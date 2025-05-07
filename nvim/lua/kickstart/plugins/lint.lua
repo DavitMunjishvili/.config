3327d5c3941931404a6f1068ab71751250bb8d1d
Data Munjishvili <drummerthree@gmail.com>
2025-05-07 11:06:20 +0400

    use ruff for linting

--- nvim/lua/kickstart/plugins/lint.lua
+++ nvim/lua/kickstart/plugins/lint.lua
@@ -7,6 +7,7 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = lint.linters_by_ft or {}
       lint.linters_by_ft['markdown'] = { 'markdownlint-cli2' }
+      lint.linters_by_ft['python'] = { 'ruff' }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
