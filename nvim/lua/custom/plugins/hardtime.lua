893d293add2243edff53c4fe711e6c306b907de9
Data Munjishvili <drummerthree@gmail.com>
2025-06-13 15:06:32 +0400

    disable hardtime for now

--- nvim/lua/custom/plugins/hardtime.lua
+++ nvim/lua/custom/plugins/hardtime.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    'm4xshen/hardtime.nvim',
-    lazy = false,
-    dependencies = { 'MunifTanjim/nui.nvim' },
-    opts = {
-      disabled_keys = {
-        ['<Up>'] = false, -- Allow <Up> key
-        ['<Down>'] = false, -- Allow <Down> key
-        ['<Left>'] = false, -- Allow <Left> key
-        ['<Right>'] = false, -- Allow <Right> key
-      },
-    },
-  },
+  -- {
+  --   'm4xshen/hardtime.nvim',
+  --   lazy = false,
+  --   dependencies = { 'MunifTanjim/nui.nvim' },
+  --   opts = {
+  --     disabled_keys = {
+  --       ['<Up>'] = false, -- Allow <Up> key
+  --       ['<Down>'] = false, -- Allow <Down> key
+  --       ['<Left>'] = false, -- Allow <Left> key
+  --       ['<Right>'] = false, -- Allow <Right> key
+  --     },
+  --   },
+  -- },
 }
