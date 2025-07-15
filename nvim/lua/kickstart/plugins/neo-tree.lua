6308995ce2c1ff2bd58d40e8f85fdf148f7f4c4b
Data Munjishvili <drummerthree@gmail.com>
2025-07-15 18:00:16 +0400

    add support for viewing images

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -1,3 +1,30 @@
+local function preview_image(path)
+  local width = math.floor(vim.o.columns * 0.8)
+  local height = math.floor(vim.o.lines * 0.8)
+
+  local buf = vim.api.nvim_create_buf(false, true)
+  local win = vim.api.nvim_open_win(buf, true, {
+    relative = 'editor',
+    width = width,
+    height = height,
+    row = math.floor((vim.o.lines - height) / 2),
+    col = math.floor((vim.o.columns - width) / 2),
+    style = 'minimal',
+    border = 'single',
+  })
+
+  -- Mark buffer as terminal
+  vim.fn.termopen { 'viu', '-w', tostring(width - 16), path }
+
+  vim.api.nvim_buf_set_keymap(buf, 'n', 'q', '<cmd>bd!<CR>', {
+    nowait = true,
+    noremap = true,
+    silent = true,
+  })
+
+  vim.cmd 'startinsert'
+end
+
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
@@ -25,7 +52,25 @@ return {
         position = 'current',
         mappings = {
           ['\\'] = 'close_window',
+          ['o'] = 'open_with_viewer',
+          ['<cr>'] = 'open_with_viewer',
         },
+      },
+      commands = {
+        open_with_viewer = function(state)
+          local node = state.tree:get_node()
+          if node.type == 'directory' then
+            state.commands.toggle_node(state)
+          else
+            local path = node.path
+            local extension = path:match '%.([^%.]+)$'
+            if extension and vim.tbl_contains({ 'png', 'jpg', 'jpeg', 'gif', 'webp' }, extension:lower()) then
+              preview_image(path)
+            else
+              state.commands.open(state)
+            end
+          end
+        end,
       },
     },
   },
