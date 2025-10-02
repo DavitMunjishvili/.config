c4bd45e4f4b9cd044e3eb591fd8587644d7aef2f
Data Munjishvili <drummerthree@gmail.com>
2025-10-02 18:12:54 +0400

    move conform into a separate config file

--- nvim/init.lua
+++ nvim/init.lua
@@ -257,58 +257,7 @@ require('lazy').setup({
 
   { import = 'core.plugins.lsp' },
 
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-          return
-        end
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        local lsp_format_opt
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          lsp_format_opt = 'never'
-        else
-          lsp_format_opt = 'fallback'
-        end
-        return {
-          timeout_ms = 500,
-          lsp_format = lsp_format_opt,
-        }
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-
-        python = { 'black' },
-        bash = { 'shfmt' },
-
-        javascript = { 'prettier', stop_after_first = true },
-        javascriptreact = { 'prettier', stop_after_first = true },
-        typescript = { 'prettier', stop_after_first = true },
-        typescriptreact = { 'prettier', stop_after_first = true },
-        html = { 'prettier', stop_after_first = true },
-        json = { 'prettier', stop_after_first = true },
-        kotlin = { 'ktlint' },
-        markdown = { 'markdownlint-cli2' },
-      },
-    },
-  },
+  { import = 'core.plugins.conform' },
 
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
