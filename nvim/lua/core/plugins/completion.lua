3521a423160738b055efcd3524a68901762c4992
Data Munjishvili <drummerthree@gmail.com>
2025-10-02 18:39:58 +0400

    fix missing backslash

--- nvim/lua/core/plugins/completion.lua
+++ nvim/lua/core/plugins/completion.lua
@@ -91,7 +91,7 @@ return { -- Autocompletion
         -- Accept ([y]es) the completion.
         --  This will auto-import if your LSP supports it.
         --  This will expand snippets if the LSP sent a snippet.
-        ['<C-\>'] = cmp.mapping.confirm { select = true },
+        ['<C-\\>'] = cmp.mapping.confirm { select = true },
 
         -- If you prefer more traditional completion keymaps,
         -- you can uncomment the following lines
