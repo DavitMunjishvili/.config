c4198650b86af1f2478b80c84ea86d46b2fa4d85
Data Munjishvili <drummerthree@gmail.com>
2025-10-14 23:08:41 +0400

    add lsp file tree operations

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -24,54 +24,65 @@ local function preview_image(path)
 
   vim.cmd 'startinsert'
 end
-
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    {
-      '\\',
-      function()
-        vim.cmd 'Neotree reveal'
-      end,
-      desc = 'NeoTree reveal',
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim',
     },
-  },
-  ---@module "neo-tree"
-  ---@type neotree.Config?
-  opts = {
-    filesystem = {
-      hijack_netrw_behavior = 'disabled',
-      window = {
-        position = 'current',
-        mappings = {
-          ['\\'] = 'close_window',
-          ['o'] = 'open_with_viewer',
-          ['<cr>'] = 'open_with_viewer',
+    cmd = 'Neotree',
+    keys = {
+      {
+        '\\',
+        function()
+          vim.cmd 'Neotree reveal'
+        end,
+        desc = 'NeoTree reveal',
+      },
+    },
+    ---@module "neo-tree"
+    ---@type neotree.Config?
+    opts = {
+      filesystem = {
+        hijack_netrw_behavior = 'disabled',
+        window = {
+          position = 'current',
+          mappings = {
+            ['\\'] = 'close_window',
+            ['o'] = 'open_with_viewer',
+            ['<cr>'] = 'open_with_viewer',
+          },
+        },
+        commands = {
+          open_with_viewer = function(state)
+            local node = state.tree:get_node()
+            if node.type == 'directory' then
+              state.commands.toggle_node(state)
+            else
+              local path = node.path
+              local extension = path:match '%.([^%.]+)$'
+              if extension and vim.tbl_contains({ 'png', 'jpg', 'jpeg', 'gif', 'webp' }, extension:lower()) then
+                preview_image(path)
+              else
+                state.commands.open(state)
+              end
+            end
+          end,
         },
       },
-      commands = {
-        open_with_viewer = function(state)
-          local node = state.tree:get_node()
-          if node.type == 'directory' then
-            state.commands.toggle_node(state)
-          else
-            local path = node.path
-            local extension = path:match '%.([^%.]+)$'
-            if extension and vim.tbl_contains({ 'png', 'jpg', 'jpeg', 'gif', 'webp' }, extension:lower()) then
-              preview_image(path)
-            else
-              state.commands.open(state)
-            end
-          end
-        end,
-      },
     },
+  },
+  {
+    'antosha417/nvim-lsp-file-operations',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-neo-tree/neo-tree.nvim',
+    },
+    config = function()
+      require('lsp-file-operations').setup()
+    end,
   },
 }
